868be42ea643e2ce175edb0d623d5bc2763f404c
denierus <denieru@live.dk>
2016-03-03 13:40:52 +0100

    ForskydningskraftImpl update

---
+++ PTE/Implemenation/Logic/ForskydningskraftImpl.java
@@ -0,0 +1,46 @@
+package Logic;
+
+import Exceptions.UdefineretProfilException;
+
+public class ForskydningskraftImpl implements Forskydningskraft  {
+	Vinkel v;
+	DimensionerendeKraft dk;
+	public ForskydningskraftImpl(Vinkel v, DimensionerendeKraft dk){
+		this.v = v;
+		this.dk = dk;
+	}
+	double forskydningskraft;
+	
+	@Override
+	public void setForskydningskraft(double forskydningskraft) {
+		this.forskydningskraft = forskydningskraft;
+
+	}
+
+	@Override
+	public double getForskydningskraft() throws UdefineretProfilException {
+		double vinkel = v.getVinkel();
+		Profil profil = v.getProfil();
+		
+		if (profil == Profil.VANDRET){
+			return (Math.cos(vinkel)*dk.getDimensionerendeKraft());
+		}
+		else if (profil == Profil.LODRET){
+			return (Math.sin(vinkel)*dk.getDimensionerendeKraft());
+		}
+		else throw new UdefineretProfilException("Udefineret Profil");
+	}
+	double getForskydningskraft(double vinkel, double dimensionerendeKraft) {
+		forskydningskraft = Math.cos(vinkel) * dimensionerendeKraft;
+
+		return forskydningskraft;
+	}
+
+	@Override
+	public void nulstil() {
+		//SD ikke fundet, lavet ud fra gætværk
+		forskydningskraft = 0;
+	}
+	
+	
+}
