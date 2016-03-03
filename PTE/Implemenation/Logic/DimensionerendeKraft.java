29b30a6c3156b3f788e24ba3b36b93aafaa6127a
Zeltres <zeltr@DESKTOP-J0IIDR8.ceuherning.int>
2016-03-03 09:34:06 +0100

    Dimensionerendekraft genimplementeret

---
+++ PTE/Implemenation/Logic/DimensionerendeKraft.java
@@ -0,0 +1,10 @@
+package Logic;
+
+public interface DimensionerendeKraft {
+
+	public double getDimensionerendeKraft();
+	
+	public void setDimensionerendeKraft(double dimensionerendeKraft);
+	
+	public void nulstil();
+}
