f370b31a4ba7338075fe4218989bad826399ac62
denierus <denieru@live.dk>
2016-03-03 13:38:49 +0100

    update i forskydningskraftimpl + test

--- PTE/Test/Logic/ForskydningskraftTest.java
+++ PTE/Test/Logic/ForskydningskraftTest.java
@@ -7,93 +7,13 @@ import org.junit.Test;
 
 	public class ForskydningskraftTest {
 
-		
-
-		
-
-		public class TyngdekraftStub implements Tyngdekraft {
-
-			@Override
-			public double getTyngdekraft() {
-				
-				return 9.816;
-			}
-
-			@Override
-			public void setTyngdekraft(double tyngdekraft) {
-				fail();
-
-			}
-
-			@Override
-			public void nulstil() {
-			fail();
-
-			}
-
-		}
-
 		@Test
-		public void testDimensionerendeKraftTiKg() {
-			Tyngdekraft tyngdekraft = new TyngdekraftStub();
-			Vaegt vaegt = new VaegtStubTiKg();
-			DimensionerendeKraft dk = new DimensionerendeKraftImpl(vaegt, tyngdekraft);
-			
-			assertEquals(98.16,dk.getDimensionerendeKraft(),0.001);
-			
-			 
+		public void testGetNormalkraft() {
+			ForskydningskraftImpl nk = new ForskydningskraftImpl(new VinkelImpl(), 
+						     new DimensionerendeKraftImpl(new VaegtImpl(), new TyngdekraftImpl()));
+			assertEquals(3788.842, nk.getForskydningskraft(50,3926.4), 0.001);
+			assertEquals(3926.398, nk.getForskydningskraft(0.001,3926.4), 0.001);
+			assertEquals(-1755.805, nk.getForskydningskraft(89.999,3926.4), 0.001);
 		}
-		public class VaegtStubTiKg implements Vaegt {
-
-			@Override
-			public double getVaegt() {
-			
-				return 10;
-			}
-
-			@Override
-			public void setVaegt(double vaegt) {
-				fail();
-
-			}
-
-			@Override
-			public void nulstil() {
-				fail();
-
-			}
-
-		}
-		
-		@Test
-		public void testDimensionerendeKraftHalvKg() {
-			Tyngdekraft tyngdekraft = new TyngdekraftStub();
-			Vaegt vaegt = new VaegtStubHalvKg();
-			DimensionerendeKraft dk = new DimensionerendeKraftImpl(vaegt, tyngdekraft);
-			
-			assertEquals(4.908,dk.getDimensionerendeKraft(),0.001);
-			
-			
-		}
-		public class VaegtStubHalvKg implements Vaegt {
-
-			@Override
-			public double getVaegt() {
-				
-				return 0.5;
-			}
-
-			@Override
-			public void setVaegt(double vaegt) {
-				fail();
-			}
-
-			@Override
-			public void nulstil() {
-				fail();
-
-			}
-	}
-	
 	
 }
